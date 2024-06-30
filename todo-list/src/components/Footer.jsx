1e66df41a81729a9fe746382f44c0f8e435f728a
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-06-30 17:46:16 -0600

    fix warning console

--- todo-list/src/components/Footer.jsx
+++ todo-list/src/components/Footer.jsx
@@ -18,11 +18,11 @@ export default function Footer() {
                 width="24"
                 height="24"
                 viewBox="0 0 24 24"
-                stroke-width="2"
+                strokeWidth="2"
                 stroke="currentColor"
                 fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               >
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                 <path d="M11.5 17h-7.5a1 1 0 0 1 -1 -1v-12a1 1 0 0 1 1 -1h16a1 1 0 0 1 1 1v9"></path>
