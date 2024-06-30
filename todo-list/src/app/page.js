3a29616ea3c9110ba216525eca4eca8ecc9d2d51
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-06-29 19:27:04 -0600

    first commit- working with next and bootstrap

---
+++ todo-list/src/app/page.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="h1">Add Bootstrap in Next.js</div>
+      </div>
+
+      <div className="row">
+        <div className="col-12">
+          <p>How install Bootstrap in Next.js App</p>
+        </div>
+      </div>
+    </div>
+  );
+}
