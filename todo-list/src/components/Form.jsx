fef5894d02034d3ed424493f30efaacbb6c07826
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-06-30 13:49:58 -0600

    add svg add/clean in input

--- todo-list/src/components/Form.jsx
+++ todo-list/src/components/Form.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 export default function Form() {
   return (
@@ -9,12 +10,32 @@ export default function Form() {
         required
         className="form-control w-50 mx-3"
       />
-      <div className="d-flex gap-3">
-        <button type="submit" className="btn btn-outline-secondary">
-          Agregar
+      <div className="d-flex gap-2">
+        <select class="form-select" aria-label="Default select example">
+          <option selected>Prioridad</option>
+          <option value="1">Alta</option>
+          <option value="2">Media</option>
+          <option value="3">Baja</option>
+        </select>
+        <button type="submit" className="btn btn-light btn-sm">
+          <Image
+            src="/assets/add.svg"
+            alt="Add"
+            type="button"
+            width={30}
+            height={30}
+            className=""
+          />
         </button>
-        <button type="button" className="btn btn-outline-secondary">
-          Borrar
+        <button type="button" className="btn btn-light btn-sm">
+          <Image
+            src="/assets/clean.svg"
+            alt="Add"
+            type="button"
+            width={30}
+            height={30}
+            className=""
+          />
         </button>
       </div>
     </form>
