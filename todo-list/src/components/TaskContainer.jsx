d1569e6e6a4957b3d37a33d523253fdc68df5cc0
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-06-30 14:21:21 -0600

    add tackContainer

---
+++ todo-list/src/components/TaskContainer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import TaskTable from "./TaskTable";
+import Image from "next/image";
+
+export default function TaskContainer() {
+  return (
+    <div className="container">
+      <div className="row d-flex justify-content-center align-items-center py-4 w-25">
+        <div className="d-flex align-items-center">
+          <Image
+            src="/assets/filter.svg"
+            alt="Filtrar"
+            type="button"
+            width={30}
+            height={30}
+            className="me-2" // Clase para margen a la derecha
+          />
+          <select
+            className="form-select form-select-sm"
+            aria-label="Default select example"
+          >
+            <option selected>Mostrar</option>
+            <option value="1">Todas</option>
+            <option value="2">Completadas</option>
+            <option value="3">Pendientes</option>
+          </select>
+        </div>
+      </div>
+      <TaskTable />
+    </div>
+  );
+}
