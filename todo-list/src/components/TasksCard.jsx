efe0ce66c92a69dbcf763914abe3c15d298764c5
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-06-30 13:50:34 -0600

    create tarkCard to tasks

---
+++ todo-list/src/components/TasksCard.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Image from "next/image";
+
+export default function TasksCard({ num, task, priority }) {
+  return (
+    <tr className="fw-normal">
+      <th>
+        <span className="ms-2">{num}</span>
+      </th>
+      <td className="align-middle">
+        <span>{task}</span>
+      </td>
+      <td className="align-middle">
+        <h6 className="mb-0">
+          <span className={`badge ${priority === 'alta' ? 'bg-danger' : priority === 'media' ? 'bg-warning' : 'bg-success'}`}>
+            {priority === 'alta' ? 'High priority' : priority === 'media' ? 'Medium priority' : 'Low priority'}
+          </span>
+        </h6>
+      </td>
+      <td className="align-middle">
+        <button title="Hecho" className="btn btn-success gap-2">
+        <Image
+            src="/assets/success.svg"
+            alt="Add"
+            type="button"
+            width={30}
+            height={30}
+            className="text-white"
+          />
+        </button>
+        <button title="Eliminar" className="btn btn-danger">
+        <Image
+            src="/assets/delete.svg"
+            alt="Add"
+            type="button"
+            width={30}
+            height={30}
+            className="text-white"
+          />
+        </button>
+      </td>
+    </tr>
+  );
+}
