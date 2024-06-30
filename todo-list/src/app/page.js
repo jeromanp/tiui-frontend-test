7e9972e8dca1ecf34b0c2c3370f9cd87e1f0ee1a
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-06-30 02:44:12 -0600

    change title and short description/ add compoenet Form

--- todo-list/src/app/page.js
+++ todo-list/src/app/page.js
@@ -1,18 +1,23 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Form from "@/components/Form";
 
 export default function Home() {
   return (
     <div className="container h-screen">
       <div className="row">
-        <div className="h1">Add Bootstrap in Next.js</div>
+        <h1 className="h1 #B46558 text-center">
+          <span className="gradient">To Do List App</span>
+        </h1>
       </div>
 
       <div className="row">
         <div className="col-12">
-          <p>How install Bootstrap in Next.js App</p>
+          <p className="h5 d-flex justify-content-center py-4">
+            Utiliza esta aplicación para crear una lista de tareas por hacer en
+            tu día a día.
+          </p>
         </div>
       </div>
+      <Form />
     </div>
   );
 }
