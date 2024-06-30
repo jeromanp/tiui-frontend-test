50c0f186d04fc0f694c1ed8c9d758e5bbc6700ab
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-06-30 14:21:05 -0600

    create tableTask

--- todo-list/src/components/TaskTable.jsx
+++ todo-list/src/components/TaskTable.jsx
@@ -7,13 +7,13 @@ const taskList = {
     { num: 2, task: "Tarea de ejemplo 2", priority: "media" },
     { num: 3, task: "Tarea de ejemplo 3", priority: "baja" },
     { num: 4, task: "Tarea de ejemplo 4", priority: "media" },
-    { num: 5, task: "Tarea de ejemplo 5", priority: "alta" }
-  ]
+    { num: 5, task: "Tarea de ejemplo 5", priority: "alta" },
+  ],
 };
 
 export default function TaskTable() {
   return (
-    <div className="container py-5 h-100">
+    <div className="container h-100">
       <div className="row d-flex justify-content-center align-items-center h-100">
         <table className="table text-white mb-0">
           <thead>
@@ -26,7 +26,12 @@ export default function TaskTable() {
           </thead>
           <tbody>
             {taskList.tasks.map((task, index) => (
-              <TasksCard key={index} num={task.num} task={task.task} priority={task.priority} />
+              <TasksCard
+                key={index}
+                num={task.num}
+                task={task.task}
+                priority={task.priority}
+              />
             ))}
           </tbody>
         </table>
