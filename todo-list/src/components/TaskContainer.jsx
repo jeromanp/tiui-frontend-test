b2c44b54bfb06ad70474c9c7998b080e665abf16
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-06-30 21:42:33 -0600

    separate select filter

--- todo-list/src/components/TaskContainer.jsx
+++ todo-list/src/components/TaskContainer.jsx
@@ -5,28 +5,7 @@ import Form from "./Form";
 export default function TaskContainer() {
   return (
     <div className="container">
-      <Form />
-      <div className="row d-flex justify-content-center align-items-center py-4 w-25">
-        <div className="d-flex align-items-center">
-          <Image
-            src="/assets/filter.svg"
-            alt="Filtrar"
-            type="button"
-            width={30}
-            height={30}
-            className="me-2"
-          />
-          <select
-            className="form-select form-select-sm"
-            aria-label="Default select example"
-          >
-            <option selected>Mostrar</option>
-            <option value="1">Todas</option>
-            <option value="2">Completadas</option>
-            <option value="3">Pendientes</option>
-          </select>
-        </div>
-      </div>
+      <Form />      
     </div>
   );
 }
