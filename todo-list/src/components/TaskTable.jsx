2386991ef122adbe11457ea49b175151f40a4e42
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-06-30 19:16:24 -0600

    change name properties

--- todo-list/src/components/TaskTable.jsx
+++ todo-list/src/components/TaskTable.jsx
@@ -3,33 +3,33 @@ import TasksCard from "./TasksCard";
 
 const taskList = {
   tasks: [
-    { num: 1, task: "Tarea de ejemplo 1", priority: "alta" },
-    { num: 2, task: "Tarea de ejemplo 2", priority: "media" },
-    { num: 3, task: "Tarea de ejemplo 3", priority: "baja" },
-    { num: 4, task: "Tarea de ejemplo 4", priority: "media" },
-    { num: 5, task: "Tarea de ejemplo 5", priority: "alta" },
+    { id: 1, task: "Tarea de ejemplo 1", priority: "alta", completed: false },
+    { id: 2, task: "Tarea de ejemplo 2", priority: "media", completed: false },
+    { id: 3, task: "Tarea de ejemplo 3", priority: "baja", completed: false },
+    { id: 4, task: "Tarea de ejemplo 4", priority: "media", completed: false },
+    { id: 5, task: "Tarea de ejemplo 5", priority: "alta", completed: false },
   ],
 };
 
-export default function TaskTable() {
+export default function TaskTable({ tasks }) {
   return (
     <div className="container h-100">
       <div className="row d-flex justify-content-center align-items-center h-100">
         <table className="table text-white mb-0">
           <thead>
             <tr>
-              <th scope="col">Número</th>
+              <th scope="col">ID</th>
               <th scope="col">Tarea</th>
               <th scope="col">Prioridad</th>
               <th scope="col">Acciones</th>
             </tr>
           </thead>
           <tbody>
-            {taskList.tasks.map((task, index) => (
+            {tasks.map((task, index) => (
               <TasksCard
                 key={index}
-                num={task.num}
-                task={task.task}
+                id={task.id}
+                description={task.description}
                 priority={task.priority}
               />
             ))}
