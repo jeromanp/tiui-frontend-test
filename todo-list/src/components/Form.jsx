6d39fb86abb8496d82ad0589193ea1e1b21a5aa7
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-06-30 19:16:05 -0600

    add task

--- todo-list/src/components/Form.jsx
+++ todo-list/src/components/Form.jsx
@@ -1,44 +1,113 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import Image from "next/image";
+import TaskTable from "./TaskTable";
 
 export default function Form() {
+  const [allTasks, setAllTasks] = useState([]);
+  const [task, setTask] = useState({
+    id: 1,
+    description: "",
+    priority: "",
+    completed: false,
+  });
+
+  console.log(allTasks);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setTask((prevTask) => ({
+      ...prevTask,
+      [name]: value,  
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (task.description.trim() === "" || task.priority === "") {
+      return;
+    }
+
+    const newTask = {
+      ...task,
+      id: allTasks.length + 1,
+    };
+
+    setAllTasks((prevTasks) => [...prevTasks, newTask]);
+
+    setTask({
+      id: allTasks.length + 2,
+      description: "",
+      priority: "",
+      completed: false,
+    });
+  };
+
+  const handleClear = () => {
+    setTask({
+      id: allTasks.length + 1,
+      description: "",
+      priority: "",
+      completed: false,
+    });
+  };
+
   return (
-    <form className="container d-flex justify-content-center align-items-center">
-      <input
-        type="text"
-        placeholder="Escriba una nueva tarea..."
-        required
-        className="form-control w-50 mx-3"
-      />
-      <div className="d-flex gap-2">
-        <select class="form-select" aria-label="Default select example">
-          <option selected>Prioridad</option>
-          <option value="1">Alta</option>
-          <option value="2">Media</option>
-          <option value="3">Baja</option>
-        </select>
-        <button type="submit" className="btn btn-light btn-sm">
-          <Image
-            src="/assets/add.svg"
-            alt="Add"
+    <div>
+      <form
+        className="container d-flex justify-content-center align-items-center"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          name="description"
+          value={task.description}
+          onChange={handleChange}
+          placeholder="Escriba una nueva tarea..."
+          required
+          className="form-control w-50 mx-3"
+        />
+        <div className="d-flex gap-2">
+          <select
+            name="priority"
+            value={task.priority}
+            onChange={handleChange}
+            className="form-select"
+            aria-label="Default select example"
+          >
+            <option value="">Prioridad</option>
+            <option value="Alta">Alta</option>
+            <option value="Media">Media</option>
+            <option value="Baja">Baja</option>
+          </select>
+          <button type="submit" className="btn btn-light btn-sm">
+            <Image
+              src="/assets/add.svg"
+              alt="Add"
+              type="button"
+              width={30}
+              height={30}
+              className=""
+            />
+          </button>
+          <button
             type="button"
-            width={30}
-            height={30}
-            className=""
-          />
-        </button>
-        <button type="button" className="btn btn-light btn-sm">
-          <Image
-            src="/assets/clean.svg"
-            alt="Add"
-            type="button"
-            width={30}
-            height={30}
-            className=""
-          />
-        </button>
-      </div>
-    </form>
+            className="btn btn-light btn-sm"
+            onClick={handleClear}
+          >
+            <Image
+              src="/assets/clean.svg"
+              alt="Clear"
+              type="button"
+              width={30}
+              height={30}
+              className=""
+            />
+          </button>
+        </div>
+      </form>
+      <TaskTable tasks={allTasks} />
+    </div>
   );
 }
