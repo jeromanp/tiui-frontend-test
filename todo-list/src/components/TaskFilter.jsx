2738a6b8ed0a8bd68c43f417532fce4b450d56c2
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-06-30 21:42:47 -0600

    create select filter

---
+++ todo-list/src/components/TaskFilter.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Image from 'next/image'
+
+export default function TaskFilter() {
+  return (
+    <div className="row d-flex justify-content-center align-items-center py-4 w-25">
+        <div className="d-flex align-items-center">
+          <Image
+            src="/assets/filter.svg"
+            alt="Filtrar"
+            type="button"
+            width={30}
+            height={30}
+            className="me-2"
+          />
+          <select
+            className="form-select form-select-sm"
+            aria-label="Default select example"
+          >
+            <option selected>Mostrar</option>
+            <option value="1">Todas</option>
+            <option value="2">Completadas</option>
+            <option value="3">Pendientes</option>
+          </select>
+        </div>
+      </div>
+  )
+}
