ab97f96db05ad291f43921175f323e9d528d701b
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-07-02 00:38:29 -0600

    message if tasks.length = 0

--- todo-list/src/components/TaskTable.jsx
+++ todo-list/src/components/TaskTable.jsx
@@ -8,32 +8,40 @@ export default function TaskTable({
   uncompleteTask,
 }) {
   return (
-    <div className="container h-100">
-      <div className="row d-flex justify-content-center align-items-center h-100">
-        <table className="table text-white mb-0 w-100">
-          <thead>
-            <tr>
-              <th scope="col">ID</th>
-              <th className="w-100" scope="col">
-                Tarea
-              </th>
-              <th scope="col">Prioridad</th>
-              <th scope="col">Acciones</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tasks.map((task) => (
-              <TasksCard
-                key={task.id}
-                {...task}
-                completeTask={completeTask}
-                deleteTask={deleteTask}
-                uncompleteTask={uncompleteTask}
-              />
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
+    <>
+      {tasks.length === 0 ? (
+        <div className="d-flex justify-content-center align-items-center">
+          <h3>No hay tareas que mostrar</h3>
+        </div>
+      ) : (
+        <div className="container h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <table className="table text-white mb-0 w-100">
+              <thead>
+                <tr>
+                  <th scope="col">ID</th>
+                  <th className="w-100" scope="col">
+                    Tarea
+                  </th>
+                  <th scope="col">Prioridad</th>
+                  <th scope="col">Acciones</th>
+                </tr>
+              </thead>
+              <tbody>
+                {tasks.map((task) => (
+                  <TasksCard
+                    key={task.id}
+                    {...task}
+                    completeTask={completeTask}
+                    deleteTask={deleteTask}
+                    uncompleteTask={uncompleteTask}
+                  />
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
