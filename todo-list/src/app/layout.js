cc78f420fd30b6738d6791054b41830cb5d2d98e
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-07-01 23:41:54 -0600

    use context y layuout

--- todo-list/src/app/layout.js
+++ todo-list/src/app/layout.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-
+import { TasksProvider } from "./context/TaskContext";
 
 export const metadata = {
   title: "RoPi ToDo List",
@@ -14,13 +14,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <body>
-        <header>
-          <Navbar />
-        </header>
-        <main>{children}</main>
-        <footer>
-          <Footer/>
-        </footer>
+        <TasksProvider>
+          <header>
+            <Navbar />
+          </header>
+          <main>{children}</main>
+          <footer>
+            <Footer />
+          </footer>
+        </TasksProvider>
       </body>
     </html>
   );
