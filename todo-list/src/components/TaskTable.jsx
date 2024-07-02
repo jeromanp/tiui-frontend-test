657b99e65ca630bafbb6728d56ed567481d5e014
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-07-01 19:19:48 -0600

    del data inncesecary

--- todo-list/src/components/TaskTable.jsx
+++ todo-list/src/components/TaskTable.jsx
@@ -1,16 +1,6 @@
 import React from "react";
 import TasksCard from "./TasksCard";
 
-const taskList = {
-  tasks: [
-    { id: 1, task: "Tarea de ejemplo 1", priority: "alta", completed: false },
-    { id: 2, task: "Tarea de ejemplo 2", priority: "media", completed: false },
-    { id: 3, task: "Tarea de ejemplo 3", priority: "baja", completed: false },
-    { id: 4, task: "Tarea de ejemplo 4", priority: "media", completed: false },
-    { id: 5, task: "Tarea de ejemplo 5", priority: "alta", completed: false },
-  ],
-};
-
 export default function TaskTable({ tasks, completeTask, deleteTask, uncompleteTask }) {
   return (
     <div className="container h-100">
